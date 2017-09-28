aae44c6752b9e2bd2e8e24e894d6133c5104cb7a
Laurent Nocca <lnocca@fanreact.com>
2017-09-28 08:26:58 -0500

    routing exercise

---
+++ src/app/users/user/user.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  user: {id: number, name: string};
+
+  constructor(private route: ActivatedRoute) { } // ActivatedRoute object injected in constructor will give us access to the id passed in the URL
+
+  ngOnInit() {
+    this.user = {
+      id: this.route.snapshot.params['id'],
+      name: this.route.snapshot.params['name']
+    };
+    this.route.params
+      .
+  }
+
+
+}
